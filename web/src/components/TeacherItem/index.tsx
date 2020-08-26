0e9f5385ca4326b6f457d269599a00bd06f21fee
132anderson <132anderson@gmail.com>
2020-08-26 18:31:20 -0300

    inicio da aplicação mobile

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 import './style.css'
+import api from '../../services/api'
 
 export interface Teacher {
     
@@ -19,6 +20,11 @@ interface TeacherItemProps {
     teacher: Teacher
 }
 const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+    function createNewConnection(){
+        api.post('connections',{
+            user_id:teacher.id
+        })
+    }
     return (
         <article className="teacher-item">
                     <header>
@@ -36,10 +42,13 @@ const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
                             Preço/hora
                             <strong>R$ {teacher.cost}</strong>
                         </p>
-                        <button type="button">
+                        <a target="_blank"
+                           onClick={createNewConnection}
+                           href={`https://wa.me/${teacher.whatsapp}`}>
+                           
                             <img src={whatsappIcon} alt="whatsapp"/>
                             Entrar em contato 
-                        </button>
+                        </a>
                     </footer>
                 </article>
     )
